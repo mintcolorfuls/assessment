8a2298e5feff2a8021df469826cad545ca886872
Chanokchon Wongjumpa <mint.colorfuls@gmail.com>
2022-12-24 13:53:53 +0700

    setup middleware Logger and Recover

--- server.go
+++ server.go
@@ -4,10 +4,14 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
