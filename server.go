de8613f02fec9f33ebd38f53ce6287b5d1384518
Chanokchon Wongjumpa <mint.colorfuls@gmail.com>
2022-12-24 13:51:45 +0700

    initial echo

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
